0b9dd3e1dec8abcdc408638e476dddc19dcb6767
Ishaan Pradhan <ishaan.pradhan@gmail.com>
2026-07-29 06:47:02 +0000

    File changes

--- src/data/sessions.js
+++ src/data/sessions.js
@@ -14,5 +14,20 @@ export const SESSIONS = [
   { slug: "creatin-catapults", name: "Creatin' Catapults", date: "October 27, 2025", images: [] },
   { slug: "building-boats", name: "Building Boats", date: "October 18, 2025", images: [] },
   { slug: "crashin-cars", name: "Crashin' Cars", date: "July 29, 2025", images: [] },
-  { slug: "rollin-coasters", name: "Rollin' Coasters", date: "July 21, 2025", images: [] },
+  {
+    slug: "rollin-coasters",
+    name: "Rollin' Coasters",
+    date: "July 21, 2025",
+    images: [
+      { src: "https://media.base44.com/images/public/6a66ade4f704833c85945c24/e326833be_pic1.jpg", alt: "Group of students with their cardboard roller coaster projects" },
+      { src: "https://media.base44.com/images/public/6a66ade4f704833c85945c24/23012b491_pic2.jpg", alt: "Close-up of a student-built cardboard roller coaster track" },
+      { src: "https://media.base44.com/images/public/6a66ade4f704833c85945c24/f6caca501_pic3.jpg", alt: "Three boys showing off their roller coaster model" },
+      { src: "https://media.base44.com/images/public/6a66ade4f704833c85945c24/3cd7b0374_pic4.jpg", alt: "Two girls presenting their cardboard roller coaster project" },
+      { src: "https://media.base44.com/images/public/6a66ade4f704833c85945c24/1bd0e69e9_pic5.jpg", alt: "Student leader presenting the science behind roller coasters" },
+      { src: "https://media.base44.com/images/public/6a66ade4f704833c85945c24/05f8669e6_pic6.jpg", alt: "Students grouped with their completed roller coaster builds" },
+      { src: "https://media.base44.com/images/public/6a66ade4f704833c85945c24/79ee4a412_pic7.jpg", alt: "Mentor helping students assemble a cardboard coaster" },
+      { src: "https://media.base44.com/images/public/6a66ade4f704833c85945c24/28da2cc59_pic8.jpg", alt: "Wide view of the Rollin' Coasters session workspace" },
+      { src: "https://media.base44.com/images/public/6a66ade4f704833c85945c24/51addd6b5_pic9.jpg", alt: "Three boys collaborating on a roller coaster model" },
+    ],
+  },
 ];
